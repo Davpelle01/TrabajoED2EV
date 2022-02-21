0408f5a61c4383b03e34181839eae22979c980ec
David <David@DESKTOP-7HHVM8L>
2022-02-21 23:20:31 +0100

    1 Commit

---
+++ src/DavidPellejeroVideojuegos.java
@@ -0,0 +1,16 @@
+
+public class DavidPellejeroVideojuegos {
+	public String nombreHobby;
+	public int vida;
+	public String nombreUsuario;
+	
+	public DavidPellejeroVideojuegos(String nombreHobby, int vida, String nombreUsuario) {
+		this.nombreHobby = nombreHobby;
+		this.vida = vida;
+		this.nombreUsuario = nombreUsuario;
+	}
+	public void setNombreHobby(String nombreHobby) {
+		this.nombreHobby=nombreHobby;
+	}
+	
+}
